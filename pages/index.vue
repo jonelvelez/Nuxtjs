b252a9ba4f8a7cd955d937c91468db674f5f7692
unknown <velezjonel22@gmail.com>
2024-04-07 21:06:44 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo iste voluptatum enim suscipit nesciunt provident, velit optio quos ullam quis.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
